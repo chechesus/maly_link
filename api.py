60b01b92c1e8fc4ffbbe7fea8e276ecd34500620
chehchesus <wotfrontier@gmail.com>
2023-06-26 20:00:13 +0200

    new

--- api.py
+++ api.py
@@ -1,16 +1,23 @@
+import os
 import socket
 import json
 import hashlib
 from urllib.parse import urlparse
 import redis
+from string import Template
 
 # Define socket host and port
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 80
 
-# redis client 
-REDIS_HOST = '127.0.0.1'
-REDIS_PORT = 6379
+# API url
+API_URL = os.getenv('API_URL')
+
+# redis client
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+
 
 # Create socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,17 +26,19 @@ server_socket.bind((SERVER_HOST, SERVER_PORT))
 server_socket.listen(1)
 print('Listening on port %s ...' % SERVER_PORT)
 
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+# load index page and substitute API URL
+template_data = {"api_url": API_URL}
+with open("index.html", "r") as reader:
+    src = Template(reader.read())
+    index_page = src.substitute(template_data)
 
 def response_index(response:str):
     # request na stranku pre html
-    with open("index.html") as reader:
-        response += "Content-Type: text/html; charset=UTF-8`\n\n"
-        response += reader.read()
+    response += "Content-Type: text/html; charset=UTF-8`\n\n"
+    response += index_page
     return response
-
-url_map = {}
 
 def shorten_url(request, response):
     try:
@@ -42,16 +51,15 @@ def shorten_url(request, response):
         print("Data:", data)
         long_url = data["longUrl"]
 
-        # Hash generation 
+        # Hash generation
         h = hashlib.new('sha256')
-        h.update(long_url.encode()) # String to hash 
+        h.update(long_url.encode()) # String to hash
         short_url = h.hexdigest()[:8]
 
-        # Original URL into map 
-        url_map[short_url] = long_url
+        # Original URL into map
         redis_client.mset({short_url:long_url})
 
-        response_data = {"shortUrl": "http://localhost/" + short_url}
+        response_data = {"shortUrl": API_URL + "/" + short_url}
         response_data_str = json.dumps(response_data)
         response += "Content-Type: application/json; charset=UTF-8\n"
         response += "Content-Length: " + str(len(response_data_str)) + "\n\n"
@@ -59,10 +67,9 @@ def shorten_url(request, response):
     except Exception as e:
         print("ERROR:", e)
         raise
-        
     return response
 
-while True:    
+while True:
     client_connection, client_address = server_socket.accept()
     request = client_connection.recv(4096).decode()
     print(request)
@@ -74,7 +81,7 @@ while True:
         print("CESTA:", path)
     except:
         break
-    response = 'HTTP/1.1 200 OK\n' 
+    response = 'HTTP/1.1 200 OK\n'
 
     # Function routing
     if req == "GET" and (path == "/"  or path == "/index.html"):
@@ -93,9 +100,8 @@ while True:
 
         long_url = redis_client.get(short_url)
         if long_url :
-             long_url = url_map[short_url]
              response = 'HTTP/1.1 307 Temporary Redirect\n'# Temporarely redirected message
-             response += f"Location: {str(long_url)}\n\n" #Redirects to original URL
+             response += f"Location: {long_url.decode('UTF-8')}\n\n" #Redirects to original URL
         else:
             response = 'HTTP/1.1 404 Not Found\n\n'
 
@@ -106,3 +112,4 @@ while True:
 
 # Close socket
 server_socket.close()
+
